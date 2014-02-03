80bdc04d0a4e06a79ef1bc6decff418949d1c1ec
Calvin Hansen <TheSilentGhost@gmail.com>
2014-02-02 23:49:53 -0500

    Make temp filename generic.

--- block_hash.c
+++ block_hash.c
@@ -3,6 +3,7 @@
 #include <gcrypt.h>
 
 #define BUFFERLEN 15728640
+
 int main()
 {
         const int hashLength = gcry_md_get_algo_dlen( GCRY_MD_SHA1 );
@@ -22,8 +23,7 @@ int main()
         char *buffer = (char*) malloc (sizeof(char) * BUFFERLEN);
 
         // Load file
-        //FILE *f = fopen("test_1m", "rb");       //CAA76193D76C6F3008392E249DFB1C5019857154
-        FILE *f = fopen("test_15m", "rb");      //2FEDA21AE54F64897F70F1A0947E33F4CA3CE8B9
+        FILE *f = fopen("test", "rb");
         if (f == NULL) {
                 printf("Failed to open file.");
                 exit(3);
